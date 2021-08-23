d8f9f604804fa61ffac6a0dbbc87ae0448773ba3
Douglas Ribeiro Silverio <douglas.silverio.ti@gmail.com>
2021-08-23 01:38:11 -0300

    First commit

---
+++ src/main/java/sistemavendasgenerico/HelloWorld.java
@@ -0,0 +1,31 @@
+package sistemavendasgenerico;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "helloWorld")
+@SessionScoped
+public class HelloWorld {
+	private String firstName = "John";
+	  private String lastName = "Doe";
+
+	  public String getFirstName() {
+	    return firstName;
+	  }
+
+	  public void setFirstName(String firstName) {
+	    this.firstName = firstName;
+	  }
+
+	  public String getLastName() {
+	    return lastName;
+	  }
+
+	  public void setLastName(String lastName) {
+	    this.lastName = lastName;
+	  }
+
+	  public String showGreeting() {
+	    return "Hello " + firstName + " " + lastName + "!";
+	  }
+}
