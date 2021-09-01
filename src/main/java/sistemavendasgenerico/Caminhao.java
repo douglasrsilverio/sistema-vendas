8af88cd06327e4df019f655108b90784a8139809
Douglas Ribeiro Silverio <douglas.silverio.ti@gmail.com>
2021-09-01 00:20:10 -0300

    Hibernate configurado

---
+++ src/main/java/sistemavendasgenerico/Caminhao.java
@@ -0,0 +1,27 @@
+package sistemavendasgenerico;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Caminhao {
+
+	 public Caminhao() {}
+	 
+	 Caminhao(Integer id, String placa, String marca){
+	        this.id = id;
+	        this.placa = placa;
+	        this.marca = marca;
+	 }
+	    
+	 @Id
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
+	 private Integer id;
+	 
+	 private String placa;
+
+	 private String marca;
+	
+}
--- src/main/java/sistemavendasgenerico/HelloWorld.java
+++ src/main/java/sistemavendasgenerico/HelloWorld.java
@@ -1,31 +1,56 @@
 package sistemavendasgenerico;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
 @ManagedBean(name = "helloWorld")
 @SessionScoped
 public class HelloWorld {
 	private String firstName = "John";
-	  private String lastName = "Doe";
+	private String lastName = "Doe";
 
-	  public String getFirstName() {
-	    return firstName;
-	  }
+	public HelloWorld() {
+	}
 
-	  public void setFirstName(String firstName) {
-	    this.firstName = firstName;
-	  }
+	public String getFirstName() {
+		return firstName;
+	}
 
-	  public String getLastName() {
-	    return lastName;
-	  }
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
 
-	  public void setLastName(String lastName) {
-	    this.lastName = lastName;
-	  }
+	public String getLastName() {
+		return lastName;
+	}
 
-	  public String showGreeting() {
-	    return "Hello " + firstName + " " + lastName + "!";
-	  }
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String showGreeting() {
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+
+		// provavelmente isso deveria estar na classe caminhão como um set e get
+		// Insert
+
+		Caminhao caminhao = new Caminhao(null, "ZBX-143", "SCANIA");
+
+		em.getTransaction().begin();
+		em.persist(caminhao);
+		em.getTransaction().commit();
+
+		// select
+
+		List caminhoes = em.createQuery("FROM " + Caminhao.class.getName()).getResultList();
+		System.out.println("teste");
+		
+		return "Hello " + firstName + " " + lastName + "!";
+	}
 }
